3b5764b3ec5051bff519b1ae06413be91823d020
Fraser Gordon <fraser.gordon@xvi.io>
2021-02-07 00:06:06 -0500

    WIP 2021-02-06

--- System/ABI/Atomic/Source/System/ABI/Atomic/Arch/ARM/Primitives_v8.cc
+++ System/ABI/Atomic/Source/System/ABI/Atomic/Arch/ARM/Primitives_v8.cc
@@ -11,15 +11,15 @@
 #if __ARM_ARCH >= 8
 
 
-#if 1 || __ATOMIC_ASSUME_LSE_ATOMICS
+#if __ATOMIC_ASSUME_LSE_ATOMICS
 
-#define __AARCH64_CAS(reg, size, order, size_suffix, order_suffix) \
-extern "C" __uint##size##_t __aarch64_cas##size##_##order(__uint##size##_t expect, __uint##size##_t desire, __uint##size##_t* ptr); \
-__uint##size##_t __aarch64_cas##size##_##order(__uint##size##_t expect, __uint##size##_t desire, __uint##size##_t* ptr) \
+#define __AARCH64_CAS(reg, size, size_name, order, size_suffix, order_suffix) \
+extern "C" __uint##size##_t __aarch64_cas##size_name##_##order(__uint##size##_t expect, __uint##size##_t desire, __uint##size##_t* ptr); \
+__uint##size##_t __aarch64_cas##size_name##_##order(__uint##size##_t expect, __uint##size##_t desire, __uint##size##_t* ptr) \
 { \
     asm volatile \
     ( \
-        "cas" order_suffix size_suffix " %" reg "[expect], %" reg "[desire], [%[ptr]]" \
+        "CAS" order_suffix size_suffix " %" reg "[expect], %" reg "[desire], [%[ptr]]" \
         :   [expect] "+r" (expect)\
         :   "0" (expect), \
             [desire] "r" (desire), \
@@ -29,14 +29,14 @@ __uint##size##_t __aarch64_cas##size##_##order(__uint##size##_t expect, __uint##
     return expect; \
 }
 
-#define __AARCH64_SWP(reg, size, order, size_suffix, order_suffix) \
-extern "C" __uint##size##_t __aarch64_swp##size##_##order(__uint##size##_t desire, __uint##size##_t* ptr); \
-__uint##size##_t __aarch64_swp##size##_##order(__uint##size##_t desire, __uint##size##_t* ptr) \
+#define __AARCH64_SWP(reg, size, size_name, order, size_suffix, order_suffix) \
+extern "C" __uint##size##_t __aarch64_swp##size_name##_##order(__uint##size##_t desire, __uint##size##_t* ptr); \
+__uint##size##_t __aarch64_swp##size_name##_##order(__uint##size##_t desire, __uint##size##_t* ptr) \
 { \
     __uint##size##_t previous; \
     asm volatile \
     ( \
-        "swp" order_suffix size_suffix " %" reg "[desire], %" reg "[previous], [%[ptr]]" \
+        "SWP" order_suffix size_suffix " %" reg "[desire], %" reg "[previous], [%[ptr]]" \
         :   [previous] "=r" (previous) \
         :   [desire] "r" (desire), \
             [ptr] "r" (ptr) \
@@ -45,14 +45,14 @@ __uint##size##_t __aarch64_swp##size##_##order(__uint##size##_t desire, __uint##
     return previous; \
 }
 
-#define __AARCH64_LDOP(op, opcode, reg, size, order, size_suffix, order_suffix) \
-extern "C" __uint##size##_t __aarch64_ld##op##size##_##order(__uint##size##_t operand, __uint##size##_t* ptr); \
-__uint##size##_t __aarch64_ld##op##size##_##order(__uint##size##_t operand, __uint##size##_t* ptr) \
+#define __AARCH64_LDOP(op, opcode, reg, size, size_name, order, size_suffix, order_suffix) \
+extern "C" __uint##size##_t __aarch64_ld##op##size_name##_##order(__uint##size##_t operand, __uint##size##_t* ptr); \
+__uint##size##_t __aarch64_ld##op##size_name##_##order(__uint##size##_t operand, __uint##size##_t* ptr) \
 { \
     __uint##size##_t previous; \
     asm volatile \
     ( \
-        "ld" opcode order_suffix size_suffix " %" reg "[operand], %" reg "[previous], [%[ptr]]" \
+        "LD" opcode order_suffix size_suffix " %" reg "[operand], %" reg "[previous], [%[ptr]]" \
         :   [previous] "=r" (previous) \
         :   [operand] "r" (operand), \
             [ptr] "r" (ptr) \
@@ -61,45 +61,152 @@ __uint##size##_t __aarch64_ld##op##size##_##order(__uint##size##_t operand, __ui
     return previous; \
 }
 
-#define __AARCH64_LDADD(reg, size, order, size_suffix, order_suffix) \
-    __AARCH64_LDOP(add, "add", reg, size, order, size_suffix, order_suffix)
+#define __AARCH64_LDADD(reg, size, size_name, order, size_suffix, order_suffix) \
+    __AARCH64_LDOP(add, "ADD", reg, size, size_name, order, size_suffix, order_suffix)
 
-#define __AARCH64_LDEOR(reg, size, order, size_suffix, order_suffix) \
-    __AARCH64_LDOP(eor, "eor", reg, size, order, size_suffix, order_suffix)
+#define __AARCH64_LDEOR(reg, size, size_name, order, size_suffix, order_suffix) \
+    __AARCH64_LDOP(eor, "EOR", reg, size, size_name, order, size_suffix, order_suffix)
 
-#define __AARCH64_LDCLR(reg, size, order, size_suffix, order_suffix) \
-    __AARCH64_LDOP(clr, "clr", reg, size, order, size_suffix, order_suffix)
+#define __AARCH64_LDCLR(reg, size, size_name, order, size_suffix, order_suffix) \
+    __AARCH64_LDOP(clr, "CLR", reg, size, size_name, order, size_suffix, order_suffix)
 
-#define __AARCH64_LDSET(reg, size, order, size_suffix, order_suffix) \
-    __AARCH64_LDOP(set, "set", reg, size, order, size_suffix, order_suffix)
+#define __AARCH64_LDSET(reg, size, size_name, order, size_suffix, order_suffix) \
+    __AARCH64_LDOP(set, "SET", reg, size, size_name, order, size_suffix, order_suffix)
 
-#else
+#define __AARCH64_LSE_OPS_0(reg, size, size_name, order, size_suffix, order_suffix) \
+    __AARCH64_CAS(reg, size, size_name, order, size_suffix, order_suffix) \
+    __AARCH64_SWP(reg, size, size_name, order, size_suffix, order_suffix) \
+    __AARCH64_LDADD(reg, size, size_name, order, size_suffix, order_suffix) \
+    __AARCH64_LDEOR(reg, size, size_name, order, size_suffix, order_suffix) \
+    __AARCH64_LDCLR(reg, size, size_name, order, size_suffix, order_suffix) \
+    __AARCH64_LDSET(reg, size, size_name, order, size_suffix, order_suffix)
 
-#endif
+#define __AARCH64_LSE_OPS_1(reg, size, size_name, size_suffix) \
+    __AARCH64_LSE_OPS_0(reg, size, size_name, relax, size_suffix, "") \
+    __AARCH64_LSE_OPS_0(reg, size, size_name, acq, size_suffix, "A") \
+    __AARCH64_LSE_OPS_0(reg, size, size_name, rel, size_suffix, "L") \
+    __AARCH64_LSE_OPS_0(reg, size, size_name, acq_rel, size_suffix, "AL")
 
-
-#define __AARCH64_OPS_0(reg, size, order, size_suffix, order_suffix) \
-    __AARCH64_CAS(reg, size, order, size_suffix, order_suffix) \
-    __AARCH64_SWP(reg, size, order, size_suffix, order_suffix) \
-    __AARCH64_LDADD(reg, size, order, size_suffix, order_suffix) \
-    __AARCH64_LDEOR(reg, size, order, size_suffix, order_suffix) \
-    __AARCH64_LDCLR(reg, size, order, size_suffix, order_suffix) \
-    __AARCH64_LDSET(reg, size, order, size_suffix, order_suffix)
-
-#define __AARCH64_OPS_1(reg, size, size_suffix) \
-    __AARCH64_OPS_0(reg, size, relax, size_suffix, "") \
-    __AARCH64_OPS_0(reg, size, acq, size_suffix, "a") \
-    __AARCH64_OPS_0(reg, size, rel, size_suffix, "l") \
-    __AARCH64_OPS_0(reg, size, acq_rel, size_suffix, "al")
-
-#define __AARCH64_OPS \
-    __AARCH64_OPS_1("w", 8, "b") \
-    __AARCH64_OPS_1("w", 16, "h") \
-    __AARCH64_OPS_1("w", 32, "") \
-    __AARCH64_OPS_1("x", 64, "") \
+#define __AARCH64_LSE_OPS \
+    __AARCH64_LSE_OPS_1("w", 8, 1, "B") \
+    __AARCH64_LSE_OPS_1("w", 16, 2, "H") \
+    __AARCH64_LSE_OPS_1("w", 32, 4, "") \
+    __AARCH64_LSE_OPS_1("x", 64, 8, "") \
 
 
-__AARCH64_OPS
+#else // __ATOMIC_ASSUME_LSE_ATOMICS
+
+#define __AARCH64_CAS(reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix) \
+extern "C" __uint##size##_t __aarch64_cas##size_name##_##order(__uint##size##_t expect, __uint##size##_t desire, __uint##size##_t* ptr); \
+__uint##size##_t __aarch64_cas##size_name##_##order(__uint##size##_t expect, __uint##size##_t desire, __uint##size##_t* ptr) \
+{ \
+    __uint##size##_t previous; \
+    __uint##size##_t temp; \
+    asm volatile \
+    ( \
+        "1:\n" \
+        "LD" acq_suffix "XR" size_suffix " %" reg "[previous], [%[ptr]]\n" \
+        "CMP %" reg "[previous], %" reg "[expect]\n" \
+        "B.NE 2f\n" \
+        "ST" rel_suffix "XR" size_suffix " %w[temp], %" reg "[desire], [%[ptr]]\n" \
+        "CMP %w[temp], #1\n" \
+        "B.NE 1b\n" \
+        "2:\n" \
+        :   [previous] "=&r" (previous), \
+            [temp] "=&r" (temp) \
+        :   [expect] "r" (expect), \
+            [desire] "r" (desire), \
+            [ptr] "r" (ptr) \
+        : "memory" \
+    ); \
+    return previous; \
+}
+
+#define __AARCH64_SWP(reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix) \
+extern "C" __uint##size##_t __aarch64_swp##size_name##_##order(__uint##size##_t desire, __uint##size##_t* ptr); \
+__uint##size##_t __aarch64_swp##size_name##_##order(__uint##size##_t desire, __uint##size##_t* ptr) \
+{ \
+    __uint##size##_t previous; \
+    __uint##size##_t temp; \
+    asm volatile \
+    ( \
+        "1:\n" \
+        "LD" acq_suffix "XR" size_suffix " %" reg "[previous], [%[ptr]]\n" \
+        "ST" rel_suffix "XR" size_suffix " %w[temp], %" reg "[desire], [%[ptr]]\n" \
+        "CMP %w[temp], #1\n" \
+        "B.NE 1b\n" \
+        "2:\n" \
+        :   [previous] "=&r" (previous), \
+            [temp] "=&r" (temp) \
+        :   [desire] "r" (desire), \
+            [ptr] "r" (ptr) \
+        : "memory" \
+    ); \
+    return previous; \
+}
+
+#define __AARCH64_LDOP(op_name, op, reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix) \
+extern "C" __uint##size##_t __aarch64_ld##op_name##size_name##_##order(__uint##size##_t operand, __uint##size##_t* ptr); \
+__uint##size##_t __aarch64_ld##op_name##size_name##_##order(__uint##size##_t operand, __uint##size##_t* ptr) \
+{ \
+    __uint##size##_t previous; \
+    __uint##size##_t temp1, temp2; \
+    asm volatile \
+    ( \
+        "1:\n" \
+        "LD" acq_suffix "XR" size_suffix " %" reg "[previous], [%[ptr]]\n" \
+        op " %" reg "[temp1], %" reg "[previous], %" reg "[operand]\n" \
+        "ST" rel_suffix "XR" size_suffix " %w[temp2], %" reg "[temp1], [%[ptr]]\n" \
+        "CMP %w[temp2], #1\n" \
+        "B.NE 1b\n" \
+        "2:\n" \
+        :   [previous] "=&r" (previous), \
+            [temp1] "=&r" (temp1), \
+            [temp2] "=&r" (temp2) \
+        :   [operand] "r" (operand), \
+            [ptr] "r" (ptr) \
+        : "memory" \
+    ); \
+    return previous; \
+}
+
+#define __AARCH64_LDADD(reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix) \
+    __AARCH64_LDOP(add, "ADD", reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix)
+
+#define __AARCH64_LDEOR(reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix) \
+    __AARCH64_LDOP(eor, "EOR", reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix)
+
+#define __AARCH64_LDCLR(reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix) \
+    __AARCH64_LDOP(clr, "BIC", reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix)
+
+#define __AARCH64_LDSET(reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix) \
+    __AARCH64_LDOP(set, "ORR", reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix)
+
+#define __AARCH64_LSE_OPS_0(reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix) \
+    __AARCH64_CAS(reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix) \
+    __AARCH64_SWP(reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix) \
+    __AARCH64_LDADD(reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix) \
+    __AARCH64_LDEOR(reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix) \
+    __AARCH64_LDCLR(reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix) \
+    __AARCH64_LDSET(reg, size, size_name, order, size_suffix, acq_suffix, rel_suffix)
+
+#define __AARCH64_LSE_OPS_1(reg, size, size_name, size_suffix) \
+    __AARCH64_LSE_OPS_0(reg, size, size_name, relax, size_suffix, "", "") \
+    __AARCH64_LSE_OPS_0(reg, size, size_name, acq, size_suffix, "A", "") \
+    __AARCH64_LSE_OPS_0(reg, size, size_name, rel, size_suffix, "", "L") \
+    __AARCH64_LSE_OPS_0(reg, size, size_name, acq_rel, size_suffix, "A", "L")
+
+#define __AARCH64_LSE_OPS \
+    __AARCH64_LSE_OPS_1("w", 8, 1, "B") \
+    __AARCH64_LSE_OPS_1("w", 16, 2, "H") \
+    __AARCH64_LSE_OPS_1("w", 32, 4, "") \
+    __AARCH64_LSE_OPS_1("x", 64, 8, "")
+
+
+#endif // __ATOMIC_ASSUME_LSE_ATOMICS
+
+
+__AARCH64_LSE_OPS
 
 
 namespace System::ABI::Atomic
